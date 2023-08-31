2aca53da763d9c1d029fba3bb292a852c05e4ae9
Patryk Berkowicz <patberkowicz@gmail.com>
2023-08-31 19:37:45 +0100

    added array of nav elements and hide current page

---
+++ src/modules/nav.js
@@ -0,0 +1,45 @@
+import { createElement } from "./utility/create-element.js";
+import { mainContainer } from "..";
+import { home } from "./home";
+
+const prepareNav = () => {
+    const navItems = ["home", "menu", "contact"];
+    const navContainer = createElement("nav");
+    const navUL = createElement("ul");
+  
+    // create array of nav elements (li's)
+  
+    const navElements = navItems.map((navItem) => {
+      const li = createElement("li", "", navItem);
+      li.dataset.page = navItem;
+      navUL.appendChild(li);
+      return li;
+    });
+  
+    // callback: nav item clicked
+  
+    const navigateToPage = (e) => {
+      // hide current page
+      navContainer.nextSibling.remove();
+  
+      // acquire name of page they want to visit
+      const navTarget = e.target.dataset.page;
+  
+      if (navTarget === "home") mainContainer.insertBefore(home, footer);
+      if (navTarget === "menu") mainContainer.insertBefore(menu, footer);
+      if (navTarget === "contact") mainContainer.insertBefore(contact, footer);
+    };
+  
+    // add eventlistener to all nav li elements
+  
+    navElements.forEach((element) =>
+      element.addEventListener("click", navigateToPage)
+    );
+  
+    navContainer.appendChild(navUL);
+    return navContainer;
+  };
+  
+  const nav = prepareNav();
+  
+  export { nav };
